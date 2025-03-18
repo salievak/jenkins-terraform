efe1d90143870dac661b06c670a4b1a135a92dd9
salievak <k.salieva@itoutposts.com>
2025-03-18 13:24:52 +0200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "jenkins_instance_public_ip" {
-  description = "Публічна IP-адреса Jenkins EC2 інстансу"
+  description = "Public IP address of the Jenkins EC2 instance"
   value       = aws_instance.jenkins.public_ip
 }
 
 output "jenkins_elastic_ip" {
-  description = "Еластична IP-адреса, прив'язана до інстансу"
+  description = "Elastic IP address bound to an instance"
   value       = aws_eip.jenkins.public_ip
 }
 
 output "jenkins_dns_record" {
-  description = "Повне DNS ім'я для доступу до Jenkins"
+  description = "Full DNS name to access Jenkins"
   value       = aws_route53_record.jenkins.fqdn
 }
 
 output "jenkins_url" {
-  description = "URL для доступу до Jenkins через Nginx"
+  description = "URL to access Jenkins via Nginx"
   value       = "https://${aws_route53_record.jenkins.fqdn}"
 }
 
 output "secret_manager_arn" {
-  description = "ARN секрету з паролем адміністратора Jenkins"
+  description = "ARN of the secret with Jenkins administrator password"
   value       = aws_secretsmanager_secret.jenkins_admin.arn
 }
 
 output "get_admin_password_command" {
-  description = "Команда для отримання пароля адміністратора"
+  description = "Command to get the administrator password"
   value = "aws secretsmanager get-secret-value --secret-id ${aws_secretsmanager_secret.jenkins_admin.name} --query SecretString --output text"
 }
